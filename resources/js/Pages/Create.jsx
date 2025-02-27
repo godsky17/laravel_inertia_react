b8218f72bc2ad9183f8d8786ccf5c857560b8374
godsky <gautheyelohin@gmail.com>
2025-02-28 00:51:19 +0100

    Creation de post

---
+++ resources/js/Pages/Create.jsx
@@ -0,0 +1,39 @@
+import { useForm } from "@inertiajs/react"
+
+export default function Create() {
+    const { data, setData, post, errors, processing } = useForm({
+        body: ""
+    })
+
+    function submit(e) {
+        e.preventDefault()
+        post('/posts')
+    }
+
+    return (
+        <>
+            <h1 className="title">Create a new post</h1>
+            <div className="w-1/2 mx-auto">
+                <form onSubmit={submit}>
+                    <textarea
+                        name="body"
+                        id=""
+                        rows={10} v
+                        alue={data.body}
+                        onChange={(e) => setData("body", e.target.value)}
+                        className={errors.body && '!ring-red-500'}
+                    >
+
+                    </textarea>
+                    {errors.body && <p className="error">{errors.body}</p>}
+                    <button
+                        className="primary-btn mt-4"
+                        disabled={processing}
+                    >
+                        Create post
+                    </button>
+                </form>
+            </div>
+        </>
+    )
+}
